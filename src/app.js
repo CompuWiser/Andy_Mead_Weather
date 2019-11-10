cd5e97758e3375e88bd03075341bfe92e4df5e59
CompuWiser <compuwiser@outlook.com>
2019-11-10 03:52:02 +0200

    Lesson 50: 404 page as default page

--- src/app.js
+++ src/app.js
@@ -1,31 +1,41 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('hbs');
 
 const app = express();
 
+//== Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '..', 'public');
-console.log(publicDirectoryPath);
+const partialsPath = path.join(__dirname, '../views/layouts');
+//const viewsPath = path.join(__dirname, '..', 'templates')
 
+//== Setup handlebars engine and views location
 app.set('view engine', 'hbs');
+hbs.registerPartials(partialsPath);
+//app.set('views', viewsPath)
+
+//== Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
 
 app.get('', function(req, res) {
   res.render('index', {
     title: 'Weather',
-    name: 'Andrew Mead'
+    name: 'Shanioob'
   });
 });
 
 app.get('/about', function(req, res) {
   res.render('about', {
     title: 'About Me',
-    name: 'Andrew Mead'
+    name: 'Shanioob'
   });
 });
 
 app.get('/help', function(req, res) {
   res.render('help', {
-    helpText: 'This is some helpful text.'
+    helpText: 'This is some helpful text.',
+    title: 'Help',
+    name: 'Shanioob'
   });
 });
 
@@ -35,6 +45,22 @@ app.get('/weather', function(req, res) {
     location: 'Philadelphia'
   });
 });
+
+app.get('/help/*', function(req, res) {
+  res.render('404', {
+      title: '404',
+      name: 'Andrew Mead',
+      errorMessage: 'Help article not found.'
+  })
+})
+
+app.get('*', function(req, res) {
+  res.render('404', {
+      title: '404',
+      name: 'Andrew Mead',
+      errorMessage: 'Page not found.'
+  })
+})
 
 app.listen(3000, function() {
   console.log('listening to http://localhost:3000/ or http://127.0.0.1:3000/');
