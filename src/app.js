8edefdbe9993855ce5989e5ea0d29308b5a3556d
CompuWiser <compuwiser@outlook.com>
2019-11-10 02:28:50 +0200

    Lesson 47: Using Handlebars templates to render dynamic content

--- src/app.js
+++ src/app.js
@@ -1,13 +1,35 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
 
 const publicDirectoryPath = path.join(__dirname, '..', 'public');
 console.log(publicDirectoryPath);
 
+app.set('view engine', 'hbs');
 app.use(express.static(publicDirectoryPath));
 
-app.get('/weather', (req, res) => {
+app.get('', function(req, res) {
+  res.render('index', {
+    title: 'Weather',
+    name: 'Andrew Mead'
+  });
+});
+
+app.get('/about', function(req, res) {
+  res.render('about', {
+    title: 'About Me',
+    name: 'Andrew Mead'
+  });
+});
+
+app.get('/help', function(req, res) {
+  res.render('help', {
+    helpText: 'This is some helpful text.'
+  });
+});
+
+app.get('/weather', function(req, res) {
   res.send({
     forecast: 'It is snowing',
     location: 'Philadelphia'
